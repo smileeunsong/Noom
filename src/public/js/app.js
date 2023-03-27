e09a6f40e47c76d8a8883285bb2050fb91c6920f
Eunsong Park <run.eunsong@gmail.com>
2023-03-27 18:55:58 +0900

    add: WebSocket 설정 완료

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,21 @@
-alert('hi');
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function handleOpen() {
+  console.log('Connected to Server');
+}
+
+function handleMessage(message) {
+  console.log('New message: ', message.data);
+}
+
+function handleClose() {
+  console.log('Disconnected from Server');
+}
+
+socket.addEventListener('open', handleOpen);
+socket.addEventListener('message', handleMessage);
+socket.addEventListener('close', handleClose);
+
+setTimeout(() => {
+  socket.send('hello from the browser!');
+}, 1000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -9,4 +11,25 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+// 같은 서버에서 http, webSocket 둘 다 작동시키려는 의도
+// ws만 사용하고 싶다면 http 서버를 만들지 않아도 됨
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+  console.log('Disconnected from Browser');
+}
+
+function onSocketMessage(message) {
+  console.log(message.toString());
+}
+
+wss.on('connection', (socket) => {
+  console.log('Connected to Browser');
+  socket.on('close', onSocketClose);
+  socket.on('message', onSocketMessage);
+  socket.send('hello!!');
+});
+
+server.listen(3000, handleListen);
